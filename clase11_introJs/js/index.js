a8573c06272bdef1f4707ba8e9501e6df1054292
Ana Maidana <analiamaidana@gmail.com>
2022-09-27 19:04:25 -0300

    agregando clase 10 y 11

---
+++ clase11_introJs/js/index.js
@@ -0,0 +1,9 @@
+console.log ("hola mundo");
+
+let nombre = "juan";
+let cursos = ["Prog1", "Prog2", "Prog3"];
+let aprobado = true;
+
+console.log (nombre);
+console.log (cursos);
+console.log (aprobado);
